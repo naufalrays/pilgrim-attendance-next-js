7e89612f562c7053f979716520922d370b55f19b
Ray <rayhannaufalherlano@gmail.com>
2024-05-05 20:57:42 +0700

    Fix authOptions v5

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import authOptions from "../../../../lib/authOptions";
 import NextAuth from "next-auth/next";
 
-const handler = NextAuth(authOptions) as never;
+const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+// export { handler as GET, handler as POST };
+export const GET = handler.handlers.GET;
+export const POST = handler.handlers.POST;
